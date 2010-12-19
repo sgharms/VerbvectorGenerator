fbdbac137d8cdcf97b1e18d93670c44d4c8eb7df
Steven G. Harms <sgharms@stevengharms.com>
2010-12-19 12:42:09 -0800

    About to move to a recursive setup for combi.

--- lib/verbvector.rb
+++ lib/verbvector.rb
@@ -1,5 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 require "verbvector"
+require 'pp'
 
 # Generalized module for handling lingustics processing
 module Lingustics
@@ -13,11 +14,59 @@ module Lingustics
       # given type of verb (French, Latin, Spanish).  I'm not trying to be
       # obscure, but I'm trying to be very generalizable.
       class VerbvectorGenerator
+        attr_reader :tense_list, :language
+        
+        class << self
+        end
+        
         # Initialization
         #
         # Takes the descriptive block of the tense structure in a DSL format
         def initialize(&b)
-          yield
+          @tense_list = []
+          @language   = ""
+          
+          # Let's remember the difference between instance_ and class_eval.
+          #
+          # "class_eval sets things up as if you were in the body of a class
+          # definition, so method definitions will define instance methods:"
+          #
+          # "instance_eval on a class acts as if you were working inside the
+          # singleton class of self. Therefore, any methods you define will
+          # become class methods." - prog ruby 1.9 v3, prag programmers p.388
+          #          
+          # As such, instance_eval here is about to apply this eval to the
+          # eigenclass.  That means any instance of this class will have this
+          # method.
+          instance_eval &b
+        end
+        
+        # Language takes a symbol for +l+ the language whose verb we seek to
+        # model.  It then takes a block for the sub-specification of the verbs
+        # of that language.
+        def language(l,&b) 
+          @language = l
+          # debugger
+          instance_eval &b
+        end
+        
+        # Used to take a hash that has as key the verbal aspect that is under
+        # consideration.  As values it has all the possible values that that
+        # aspect can take.  This is a wrapping method onto +start_with+ and
+        # +end_with+
+        def all_vectors(position,&b)
+          pp combinatorialize(yield)
+          
+        end
+        
+        def combinatorialize(h)
+          results = []
+          h.each_pair do |k,v|
+            v.each do |mode|
+              results.push "#{mode}_#{k}"
+            end
+          end
+          results
         end
       end
     end

df207a5f827dc772adf42d0db4612589e106b712
Steven G. Harms <sgharms@stevengharms.com>
2010-12-25 13:21:40 -0800

    Added more testing

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -5,15 +5,7 @@ require "verbvector"
 
 class TestVerbVector  < Test::Unit::TestCase
   def setup
-    @tense_list = Array.new(File.open(File.join(File.dirname(__FILE__), *%w[fixtures tense_list])).read.split /\s/)
-    @tense_list.sort!
-  end
-  def test_basics
-    v = Lingustics::Verbs::Verbvector::VerbvectorGenerator.new do
-    end
-  end
-  def test_latin
-    vv = 
+    @vv = 
     Lingustics::Verbs::Verbvector::VerbvectorGenerator.new do
        language :Latin do
          all_vectors :start_with do
@@ -48,11 +40,32 @@ class TestVerbVector  < Test::Unit::TestCase
          exception :remove, :passive_voice_imperative_mood_present_tense
        end
      end
+  end
 
-   assert_equal @tense_list, vv.tense_list
-   assert_equal 2,  vv.match_vector_upto_aspect("voice").length
-   assert_equal 5,  vv.match_vector_upto_aspect("mood").length
-   assert_equal 21, vv.match_vector_upto_aspect("tense").length
+  def test_basics
+    v = Lingustics::Verbs::Verbvector::VerbvectorGenerator.new do
+    end
+  end
 
+  def test_latin
+   tense_list = Array.new(File.open(File.join(File.dirname(__FILE__), *%w[fixtures tense_list])).read.split /\s/)
+   tense_list.sort!
+
+   assert_equal tense_list, @vv.tense_list
+   assert_equal 2,          @vv.match_vector_upto_aspect("voice").length
+   assert_equal 5,          @vv.match_vector_upto_aspect("mood").length
+   assert_equal 21,         @vv.match_vector_upto_aspect("tense").length
+  end
+  
+  def test_extension
+    m = @vv.create_module
+    k = Class.new 
+    k.class_eval do
+      include m 
+    end
+    
+    the_test = k.new
+    the_test.razzle
+         
   end
 end
